cf7166ea0e26a2b88ca903bc5cf456273e9e3872
Abdullah AlGhamdi <aalghamdi@me.com>
2021-06-03 15:23:28 +0300

    Add files via upload

---
+++ sparkpyeventskafkastreamtoconsole.py
@@ -0,0 +1,65 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col, unbase64, base64, split
+from pyspark.sql.types import StructField, StructType, StringType, BooleanType, ArrayType, DateType, FloatType
+
+# using the spark application object, read a streaming dataframe from the Kafka topic stedi-events as the source
+# Be sure to specify the option that reads all the events from the topic including those that were published before you started the spark stream
+spark = SparkSession.builder.appName("kafkaEvents").getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
+
+kafkaEventsDF = spark\
+    .readStream\
+    .format("kafka")\
+    .option("kafka.bootstrap.servers", "localhost:9092")\
+    .option("subscribe","stedi-events")\
+    .option("startingOffsets","earliest")\
+    .load()
+
+# cast the value column in the streaming dataframe as a STRING 
+kafkaEventsDF = kafkaEventsDF.selectExpr("CAST(value AS string) value")
+
+# parse the JSON from the single column "value" with a json object in it, like this:
+# +------------+
+# | value      |
+# +------------+
+# |{"custom"...|
+# +------------+
+#
+# and create separated fields like this:
+# +------------+-----+-----------+
+# |    customer|score| riskDate  |
+# +------------+-----+-----------+
+# |"sam@tes"...| -1.4| 2020-09...|
+# +------------+-----+-----------+
+#
+# storing them in a temporary view called CustomerRisk
+kafkaEventschema = StructType (
+    [
+        StructField("customer", StringType()),
+        StructField("score", FloatType()),
+        StructField("riskDate", DateType())
+    ]
+)
+kafkaEventsDF.withColumn("value", from_json("value", kafkaEventschema))\
+             .select(col('value.customer'), col('value.score'), col('value.riskDate'))\
+             .createOrReplaceTempView("CustomerRisk")
+
+# execute a sql statement against a temporary view, selecting the customer and the score from the temporary view, creating a dataframe called customerRiskStreamingDF
+sql_statement = "SELECT customer, score FROM CustomerRisk"
+customerRiskStreamingDF = spark.sql(sql_statement)
+
+# sink the customerRiskStreamingDF dataframe to the console in append mode
+# 
+# It should output like this:
+#
+# +--------------------+-----
+# |customer           |score|
+# +--------------------+-----+
+# |Spencer.Davis@tes...| 8.0|
+# +--------------------+-----
+
+customerRiskStreamingDF.writeStream.outputMode("append").format("console").start().awaitTermination()
+
+# Run the python script by running the command from the terminal:
+# /home/workspace/submit-event-kafka-streaming.sh
+# Verify the data looks correct 
